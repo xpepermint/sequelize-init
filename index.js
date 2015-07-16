ac4bf468923e68cae1b670ca020c583ff02aa5cb
xpeper <xpepermint@gmail.com>
2015-07-16 22:46:23 +0200

    adding importer logic

---
+++ index.js
@@ -0,0 +1,27 @@
+var Sequelize = require('sequelize');
+var fs = require('fs');
+var path = require('path');
+var basename = path.basename(module.filename);
+
+module.exports = function(sequelize, dir, options) {
+  var db = {};
+
+  fs.readdirSync(dir).filter(function(file) {
+    return (file.indexOf('.') !== 0) && (options.exclude||[]).indexOf(file) === -1;
+  }).forEach(function(file) {
+    if (file.slice(-3) !== '.js') return;
+    var model = sequelize['import'](path.join(dir, file));
+    db[model.name] = model;
+  });
+
+  Object.keys(db).forEach(function(modelName) {
+    if ('associate' in db[modelName]) {
+      db[modelName].associate(db);
+    }
+  });
+
+  db.sequelize = sequelize;
+  db.Sequelize = Sequelize;
+
+  return db;
+};
